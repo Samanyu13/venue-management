04aafd40e79e287aba74306e2574af772af0c6bb
Samanyu13 <samanyusaji18@gmail.com>
2019-03-25 01:45:36 +0530

    Edited login  page

---
+++ models/venue.js
@@ -0,0 +1,52 @@
+module.exports = function (sequelize, DataTypes) {
+    const Venue = sequelize
+      .define('venue', {
+        venue_id: {
+          type: DataTypes.INTEGER(),
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        venue_name: {
+          type: DataTypes.STRING(45),
+          allowNull: false,
+          unique: 'compositeIndex',
+        },
+        incharge_id: {
+          type: DataTypes.INTEGER(),
+          allowNull: false,
+          unique: 'compositeIndex',
+        },
+        event_id: {
+          type: DataTypes.INTEGER(),
+          allowNull: false,
+          unique: 'compositeIndex',
+        },
+        // implement [index6 specified in db design] compositeIndex when sequelize
+        // supports it.
+        // https://github.com/sequelize/sequelize/issues/8148
+      });
+  
+    Venue.associate = function (models) {
+      models.venue
+        .belongsTo(models.incharge, {
+          onDelete: 'CASCADE',
+          foreignKey: {
+            name: 'incharge_id',
+            // allowNull: false -- already defined
+          },
+        });
+    };
+
+    Venue.associate = function (models) {
+        models.venue
+        .belongsTo(models.event, {
+            onDelete: 'CASCADE',
+            foreignKey: {
+              name: 'event_id',
+              // allowNull: false -- already defined
+            },
+        });
+    };
+
+    return Venue;
+  };
