b0e456271ee0fc7104ec052ce0d055e693b80ae6
Samanyu13 <samanyusaji18@gmail.com>
2019-04-26 20:56:26 +0530

    Debugging

--- routes/select_venue/index.js
+++ routes/select_venue/index.js
@@ -6,16 +6,20 @@ router.get('/classes', function(req, res, next) {
   res.render('select_venue/classes', { title: 'Classes' });
 });
 
+router.get('/classes/:name', function(req, res, next) {
+  res.render('venue', { title: 'Venue'});
+});
+
 router.get('/main', function(req, res, next) {
-    res.render('select_venue/main', { title: 'Main Halls' });
-  });
+  res.render('select_venue/main', { title: 'Main Halls' });
+});
 
-  router.get('/lecture', function(req, res, next) {
-    res.render('select_venue/lecture', { title: 'Lectures' });
-  });
+router.get('/lecture', function(req, res, next) {
+  res.render('select_venue/lecture', { title: 'Lectures' });
+});
 
-  router.get('/labs', function(req, res, next) {
-    res.render('select_venue/labs', { title: 'Labs' });
-  });
+router.get('/labs', function(req, res, next) {
+  res.render('select_venue/labs', { title: 'Labs' });
+});
 
   module.exports = router;
